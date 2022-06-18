249a16dcc7d1b907d3139c27a6aac6a79cf42c2d
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-06-18 12:12:24 +0300

    Made events responsive

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 function EventCard() {
   return (
-    <div className='flex flex-col items-center justify-center gap-5 p-5 w-[30%] h-[250px] border-solid border-2 rounded-lg border-brown transition-all hover:border-lightYellow hover:shadow-xl hover:bg-[#875431]'>
+    <div className='flex flex-col items-center justify-center gap-4 p-5 w-[90%] md:w-[70%] lg:w-[50%] xl:w-[30%] md:py-10 border-solid border-2 rounded-lg border-brown transition-all hover:border-lightYellow hover:shadow-xl hover:bg-[#875431]'>
       <div className='flex flex-col items-center'>
-        <h3 className='text-lg font-extrabold'>DOUBLE MEAT NIGHT</h3>
-        <span className='text-lg'>25.06 // 18:00-22:00</span>
+        <h3 className='text-lg text-center font-extrabold'>
+          DOUBLE MEAT NIGHT
+        </h3>
+        <span className='text-lg text-center'>25.06 // 18:00-22:00</span>
       </div>
       <p className='text-center text-lg'>
         All beef patties doubled, same price.
--- components/Events.tsx
+++ components/Events.tsx
@@ -5,15 +5,15 @@ function Events() {
   return (
     <>
       <a className='anchor anchor-events' id='events'></a>
-      <div className='flex flex-col items-center gap-4 px-20 py-10 bg-brown text-lightYellow'>
-        <h1 className='text-6xl font-extrabold'>
+      <div className='flex flex-col items-center gap-8 px-2 md:px-8 lg:px-20 py-10 bg-brown text-lightYellow'>
+        <h1 className='text-4xl md:text-6xl font-extrabold text-center'>
           NEED AN EXCUSE TO LEAVE THE HOUSE?
         </h1>
-        <p className='text-xl text-center w-[70%]'>
+        <p className='text-xl text-center md:w-[80%] lg:w-[70%]'>
           Come hang out with us for a few hours. We promise it'll be worth
           putting some pants on. Bring your friends too!
         </p>
-        <div className='flex flex-col items-center gap-10 w-full pt-10'>
+        <div className='flex flex-col items-center gap-4 w-full'>
           <EventCard />
           <EventCard />
           <EventCard />

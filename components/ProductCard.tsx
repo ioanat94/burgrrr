bd01f640818fbfb9b69da7352e4c9f48cda2c894
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-07-05 10:24:57 +0300

    Fix price display on Product Card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,14 +6,9 @@ function ProductCard({ product }) {
   return (
     <Link href={`/products/${product._id}`}>
       <div className='flex flex-col items-center p-5 border-solid border-2 rounded-lg border-lightYellow  md:cursor-pointer md:transition-all md:hover:border-brown md:hover:bg-[#e8e6b3]'>
-        <Image
-          src={`/assets/burgers/${product.img}`}
-          alt=''
-          width='300'
-          height='250'
-        />
+        <Image src={product.img} alt='' width='300' height='250' />
         <h3 className='text-lg font-extrabold text-brown'>{product.title}</h3>
-        <span className='text-lg font-bold'>{`€${product.prices[0]}0`}</span>
+        <span className='text-lg font-bold'>{`€${product.prices[0]}`}</span>
         <p className='text-center text-lg'>{product.description}</p>
       </div>
     </Link>

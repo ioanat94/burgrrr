100c8970a247abaf940335303572c4b80d7d85ee
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-07-06 17:14:59 +0300

    Fix font family

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import '@fontsource/nunito/300.css';
+import '@fontsource/nunito/400.css';
+import '@fontsource/nunito/500.css';
+import '@fontsource/nunito/600.css';
+import '@fontsource/nunito/700.css';
 
 function Layout({ children }) {
   return (
-    <div className='font-nunito'>
+    <div style={{ fontFamily: 'Nunito' }}>
       <Navbar />
       {children}
       <Footer />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&display=swap'
-          rel='stylesheet'
-        />
         <link
           rel='stylesheet'
           type='text/css'
